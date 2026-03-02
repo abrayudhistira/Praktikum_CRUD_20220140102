6e7135a86293fac10da5fe1cbe391aa07e7681f8
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-02 10:43:19 +0700

    Fixed: UserRepository

--- src/main/java/com/praktikum1/repository/UserRepository.java
+++ src/main/java/com/praktikum1/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.praktikum1.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.praktikum1.model.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
