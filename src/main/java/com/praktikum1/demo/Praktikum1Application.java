d18560dfbf42f61b5e0089fdf863358be46a4dc2
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-02 09:50:16 +0700

    first commit

---
+++ src/main/java/com/praktikum1/demo/Praktikum1Application.java
@@ -0,0 +1,13 @@
+package com.praktikum1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum1/demo/Praktikum1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
