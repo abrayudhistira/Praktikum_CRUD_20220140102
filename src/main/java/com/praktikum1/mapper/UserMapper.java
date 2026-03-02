4bdccfdf829aea9226c59636ef3b06cfac137e8a
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-02 10:18:26 +0700

    implementation: UserMapper for UserDto

--- src/main/java/com/praktikum1/mapper/UserMapper.java
+++ src/main/java/com/praktikum1/mapper/UserMapper.java
@@ -1,5 +1,13 @@
 package com.praktikum1.mapper;
 
-public interface UserMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+import com.praktikum1.model.dto.UserDto;
+import com.praktikum1.model.entity.User;
+
+@Mapper
+public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+    UserDto toUserDtoData (User user);
 }
