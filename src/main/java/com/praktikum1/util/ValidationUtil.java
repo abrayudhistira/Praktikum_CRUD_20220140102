05ec96bec0bf06b90959143c4800ae24f2f76bc0
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-02 10:15:05 +0700

    implementation: validation util for request as a object

--- src/main/java/com/praktikum1/util/ValidationUtil.java
+++ src/main/java/com/praktikum1/util/ValidationUtil.java
@@ -1,6 +1,24 @@
 package com.praktikum1.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
+import jakarta.validation.ConstraintViolationException;
+
+@Component
+@AllArgsConstructor
 public class ValidationUtil {
-
+    @Autowired
+    private Validator validator;
+    public void validate(Object request) throws ConstraintViolationException {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
 }
