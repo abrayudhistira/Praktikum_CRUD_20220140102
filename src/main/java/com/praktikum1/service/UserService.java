ce919307bf6b642a165b84ea1f0c28dc153923b2
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-02 10:21:42 +0700

    implementation: UserService for basic methods

--- src/main/java/com/praktikum1/service/UserService.java
+++ src/main/java/com/praktikum1/service/UserService.java
@@ -1,5 +1,14 @@
 package com.praktikum1.service;
 
-public interface UserService {
+import java.util.List;
 
+import com.praktikum1.model.dto.UserAddRequest;
+import com.praktikum1.model.dto.UserDto;
+
+public interface UserService {
+    UserDto AddUser (UserAddRequest request);
+    List<UserDto> getAllUser();
+    UserDto getUserById(String id);
+    UserDto updateUser(String id, UserAddRequest request);
+    void DeleteUser(String id);
 }
